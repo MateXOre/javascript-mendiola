4e8195ee4c8a3f31e9b08893b1343949b8917be6
Matias Mendiola <matiasmendiola18@gmail.com>
2022-04-03 00:18:55 -0300

    subida

--- codes.js
+++ codes.js
@@ -1,4 +1,22 @@
-let objeto= prompt("¿Que le gustaria comprar?: celular ($20000), licuadora ($5000), TV ($40000).")
-let objeto2= prompt("¿Quisiera añadir algo mas?: celular ($20000), licuadora ($5000), TV ($40000).")
+let objeto
 
-function()
+const celular=20000
+const licuadora=5000
+const TV=40000
+let coste=0
+while(objeto!="no") {
+    objeto= prompt("¿Que le gustaria comprar?: celular ($20000), licuadora ($5000), TV ($40000). (Escribir 'no' si no quiere comprar mas)")
+function sumar(valor1, valor2) {
+    coste= valor1 + valor2}
+switch (objeto) {
+case "celular":sumar(coste,celular);
+alert("el coste total es $" + coste)
+break;
+case "licuadora": sumar(coste,licuadora);
+alert("el coste total es $" + coste)
+break;
+case "TV": sumar(coste,TV);
+alert("el coste total es $" + coste)
+break;
+}
+}
