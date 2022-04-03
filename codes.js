8367e236067545df004934efa36374014010093f
Matias Mendiola <matiasmendiola18@gmail.com>
2022-04-02 23:01:30 -0300

    creacion git

---
+++ codes.js
@@ -0,0 +1,4 @@
+let objeto= prompt("¿Que le gustaria comprar?: celular ($20000), licuadora ($5000), TV ($40000).")
+let objeto2= prompt("¿Quisiera añadir algo mas?: celular ($20000), licuadora ($5000), TV ($40000).")
+
+function()
